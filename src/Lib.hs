516ac4a6e86e378631b3b0e1bdb537feb2aa4e6c
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2015-11-01 22:34:28 +0100

    first compile

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, JavaScriptFFI #-}
 module Lib
     ( someFunc
     ) where
 
 import GHCJS.Foreign ()
 import GHCJS.Types
+import Data.JSString ()
 
 foreign import javascript unsafe "window.alert($1)" js_alert :: JSString -> IO ()
 
